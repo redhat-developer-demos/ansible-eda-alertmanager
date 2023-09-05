c66defce106223428f6b44cbcb21f7436815afcf
Evan Shortiss <evanshortiss@gmail.com>
2023-09-05 15:49:22 -0700

    fix: broken quarkus test

--- quarkus-monitor/src/test/java/org/acme/GreetingResourceTest.java
+++ quarkus-monitor/src/test/java/org/acme/GreetingResourceTest.java
@@ -4,7 +4,7 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.startsWith;
 
 @QuarkusTest
 public class GreetingResourceTest {
@@ -15,7 +15,7 @@ public class GreetingResourceTest {
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
+             .body(startsWith("Hello from RESTEasy Reactive"));
     }
 
 }
