a83fa3469071f643b4189211485be6c814b11f56
Evan Shortiss <evanshortiss@gmail.com>
2023-09-06 11:01:00 -0700

    feat: add scheduled taskm to gradually decrease quarkus memory gauge

--- quarkus-monitor/src/main/java/org/acme/GreetingResource.java
+++ quarkus-monitor/src/main/java/org/acme/GreetingResource.java
@@ -3,8 +3,11 @@ package org.acme;
 import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.jboss.logging.Logger;
+
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Tags;
+import io.quarkus.scheduler.Scheduled;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
@@ -13,6 +16,7 @@ import jakarta.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class GreetingResource {
+    private static final Logger Log = Logger.getLogger(GreetingResource.class);
 
     private final MeterRegistry registry;
     private AtomicInteger currentMemory;
@@ -26,10 +30,20 @@ public class GreetingResource {
         this.hostname = _hostname != null ? _hostname : "localhost";
     }
 
+    @Scheduled(every="30s")     
+    void increment() {
+        if (this.currentMemory.get() >= 1) {
+            Log.info("Decrementing memory value by 1");
+            this.currentMemory.decrementAndGet();
+            Log.infof("Memory value is now %d", this.currentMemory.get());
+        }
+    }
+
     @GET
     @Path("/consume/{amount}")
     @Produces(MediaType.TEXT_PLAIN)
     public Integer consume(@PathParam("amount") int mem) {
+        Log.infof("Increasing memory value by %d", mem);
         this.currentMemory.addAndGet(mem);
         return this.currentMemory.get();
     }
